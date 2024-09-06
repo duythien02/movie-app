b23ea866462710ec1dc39fe486c018ed2aa7ced6
Nguyen Duy Thien <thien.nguyenduy@widosoft.com>
2024-09-06 14:08:20 +0700

    config assets constant

---
+++ lib/constants/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsAppFilmRoll
+  /// assets/app/film-roll.png
+  static const String assetsAppFilmRoll = "assets/app/film-roll.png";
+}
+
