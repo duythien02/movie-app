290dca18cfae38b39bc7f801aa878aa216fd38b8
Nguyen Duy Thien <thien.nguyenduy@widosoft.com>
2024-09-08 19:53:23 +0700

    fix assets image

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
-  /// Assets for assetsAppFilmRoll
-  /// assets/app/film-roll.png
-  static const String assetsAppFilmRoll = "assets/app/film-roll.png";
-}
 
+  /// Assets for assetsAppLogo
+  /// assets/app/logo.png
+  static const String assetsAppLogo = "assets/app/logo.png";
+}
