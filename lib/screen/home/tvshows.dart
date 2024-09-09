e01ed4eb4fa51cdf9f9883b98ff1f829b3fa5f5a
Nguyen Duy Thien <thien.nguyenduy@widosoft.com>
2024-09-09 16:49:08 +0700

    feat: tv show screen

--- lib/screen/home/tvshows.dart
+++ lib/screen/home/tvshows.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../bloc/tvshows/tvshows_screen_bloc.dart';
 import '../../common/appbar.dart';
+import '../../common/list_film.dart';
 import '../../constants/app_constants.dart';
 
 class TvshowsScreen extends StatefulWidget {
@@ -11,13 +14,41 @@ class TvshowsScreen extends StatefulWidget {
 }
 
 class _TvshowsScreenState extends State<TvshowsScreen> {
+  final _tvShowsBloc = TvshowsScreenBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _tvShowsBloc.add(const InitTvShowsScreen());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(
-        child: SimpleChildAppBar(
-          titleAppBar: StringConstants.shows,
-        ),
+    return BlocProvider(
+      create: (context) => _tvShowsBloc,
+      child: BlocBuilder<TvshowsScreenBloc, TvshowsScreenState>(
+        bloc: _tvShowsBloc,
+        builder: (context, state) {
+          return Scaffold(
+            appBar: const MyAppBar(
+              child: SimpleChildAppBar(
+                titleAppBar: StringConstants.shows,
+              ),
+            ),
+            body: ListFilm(
+              isHasNetwork: state.isHasNetwork,
+              listFilm: state.tvShowsList,
+              refresh: () {
+                _tvShowsBloc.add(const InitTvShowsScreen());
+                _tvShowsBloc.refreshController.refreshCompleted();
+              },
+              loadMore: () {
+                _tvShowsBloc.add(const LoadMoreTvShows());
+              },
+              refreshController: _tvShowsBloc.refreshController,
+            ),
+          );
+        },
       ),
     );
   }
